4aac04d12140838599c884b881888814c346628f
Aaryan-Bhardwaj <aaryanbhardwaj97@gmail.com>
2021-03-03 03:40:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def create_app():
 def register_extensions(app):
 	from extensions import db  # db store to variable
 	db.init_app(app)
-	with app.app_context():
-		db.create_all()
+# 	with app.app_context():
+# 		db.create_all()
 
 
 app = create_app()
