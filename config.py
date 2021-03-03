411799ea1d90dbd02a1f1943de6fd4fd34163b7c
Aaryan-Bhardwaj <aaryanbhardwaj97@gmail.com>
2021-03-03 23:16:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///members.db'
-    SQLALCHEMY_BINDS = {'newsdb': 'sqlite:///news.db'}
+    SQLALCHEMY_BINDS = {'newsdb': 'sqlite:///news.db', 'passworddb': 'sqlite:///password.db'}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
